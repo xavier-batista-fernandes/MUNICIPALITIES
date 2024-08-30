a2b98afa4fc179a3b6d2a146bd74906bab2d1a90
Xavier Fernandes <xavier.fernandes@vwds.pt>
2024-08-30 12:08:16 +0100

    🎉 Add basic configuration

---
+++ src/app/leaflet-map/leaflet-map.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-leaflet-map',
+  standalone: true,
+  imports: [],
+  templateUrl: './leaflet-map.component.html',
+  styleUrl: './leaflet-map.component.css'
+})
+export class LeafletMapComponent {
+
+}
