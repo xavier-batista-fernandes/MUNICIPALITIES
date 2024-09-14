c01081b26a0b7e6108965e67682f75791eb4e866
Xavier Fernandes <xavier.fernandes@vwds.pt>
2024-09-14 16:32:54 +0100

    🏷 Create array of districts and function to shuffle the municipalities

--- src/app/models/district.type.ts
+++ src/app/models/district.type.ts
@@ -17,3 +17,24 @@ export type District =
     | "VIANA DO CASTELO"
     | "VILA REAL"
     | "VISEU";
+
+export const districtTypes: District[] = [
+    "AVEIRO",
+    "BEJA",
+    "BRAGA",
+    "BRAGANÇA",
+    "CASTELO BRANCO",
+    "COIMBRA",
+    "ÉVORA",
+    "FARO",
+    "GUARDA",
+    "LEIRIA",
+    "LISBOA",
+    "PORTALEGRE",
+    "PORTO",
+    "SANTARÉM",
+    "SETÚBAL",
+    "VIANA DO CASTELO",
+    "VILA REAL",
+    "VISEU",
+];
